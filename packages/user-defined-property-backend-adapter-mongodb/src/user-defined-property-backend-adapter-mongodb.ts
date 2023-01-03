b533c5dbb835797848167b695a0471334ff6aa34
Anthony Moynet <anthony.moynet@epitech.eu>
2023-01-03 14:40:32 +0530

    find

--- packages/user-defined-property-backend-adapter-mongodb/src/user-defined-property-backend-adapter-mongodb.ts
+++ packages/user-defined-property-backend-adapter-mongodb/src/user-defined-property-backend-adapter-mongodb.ts
@@ -1,14 +1,31 @@
 import { MongoClient, MongoClientOptions } from 'mongodb';
+import { UserDefinedPropertyAdapter, CreateUserDefinedPropertyInput } from '@be-metamorph/user-defined-property-shared';
+
+const COLLECTION_NAMES = {
+  USER_DEFINED_PROPERTY: 'userDefinedProperties',
+}
 
 class UserDefinedPropertyMongoDBAdapter {
   private client;
+  private userDefinedPropertyCollection;
 
   constructor(url: string, options?: MongoClientOptions) {
     this.client = new MongoClient(url, options);
+    this.userDefinedPropertyCollection = this.client.collection(COLLECTION_NAMES.USER_DEFINED_PROPERTY);
   }
 
   getClient() {
     return this.client;
+  }
+
+  async insert(input: CreateUserDefinedPropertyInput) {
+    await this.userDefinedPropertyCollection.insertOne(input);
+
+    return true;
+  }
+
+  async findById(id: string) {
+    return this.userDefinedPropertyCollection.findOne({ id });
   }
 }
 
--- packages/user-defined-property-backend/src/user-defined-property-backend.ts
+++ packages/user-defined-property-backend/src/user-defined-property-backend.ts
@@ -1,6 +1,7 @@
-import { UserDefinedPropertyAdapter, CreateUserDefinedPropertyInput } from '@be-metamorph/user-defined-property-shared';
+import { UserDefinedPropertyAdapter, CreateUserDefinedPropertyInput, UserDefinedProperty } from '@be-metamorph/user-defined-property-shared';
 
 import CreateUserDefinedProperty from './create-user-defined-property';
+import FindUserDefinedPropertyById from './find-user-defined-property-by-id';
 
 type UserDefinedPropertyBackendFacadeOptions = {
   adapter: UserDefinedPropertyAdapter;
@@ -11,10 +12,16 @@ class UserDefinedPropertyBackendFacade {
 
   constructor(options: UserDefinedPropertyBackendFacadeOptions) {}
 
-  createUserDefinedProperty(input: CreateUserDefinedPropertyInput) {
+  createUserDefinedProperty(input: CreateUserDefinedPropertyInput): boolean {
     const createUserDefinedProperty = new CreateUserDefinedProperty(this.options.adapter);
 
     return createUserDefinedProperty.execute(input);
+  }
+
+  findUserDefinedPropertyById(id: string): UserDefinedProperty {
+    const findUserDefinedPropertyById = new FindUserDefinedPropertyById(this.options.adapter);
+
+    return findUserDefinedPropertyById.execute(id);
   }
 }
 
