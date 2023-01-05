008d099b7d91bf56938f927d4f4af453f52e93d4
Anthony Moynet <anthony.moynet@epitech.eu>
2023-01-05 12:29:55 +0530

    pub

--- packages/user-defined-property-backend/src/user-defined-property-backend.ts
+++ packages/user-defined-property-backend/src/user-defined-property-backend.ts
@@ -11,7 +11,9 @@ type UserDefinedPropertyBackendFacadeOptions = {
 class UserDefinedPropertyBackendFacade {
   private options: UserDefinedPropertyBackendFacadeOptions;
 
-  constructor(options: UserDefinedPropertyBackendFacadeOptions) {}
+  constructor(options: UserDefinedPropertyBackendFacadeOptions) {
+    this.options = options;
+  }
 
   createUserDefinedProperty(input: CreateUserDefinedPropertyInput): boolean {
     const createUserDefinedProperty = new CreateUserDefinedProperty(this.options.adapter);
